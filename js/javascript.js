7aea0d2807363366df192934cfde2610e2151e85
unknown <mfkrgabriel@gmail.com>
2017-03-09 23:09:42 -0300

    working with location API

--- js/javascript.js
+++ js/javascript.js
@@ -2,29 +2,49 @@ $(document).ready(function(){
 	var $cep = $("#cep");
 	var $busca = $("#busca");
 	var $resultadoBusca = $("#resultadoBusca");
-	var url = "";//I will not show the url, because it is from a interview API
+	var url = "https://apiwv-v2.negocie.online/api/location/";  //https://apiwv-v2.negocie.online/api/location/
+	var array = {"CEP":"09725526", "lat":null, "lng":null};
+	var data = JSON.stringify(array);
+	var myCep;
+
+	function stopRefreshing (event) {
+		$cep.keypress(function(e){
+			if(e.which == 13){
+				event.preventDefault();
+			}
+		});
+	};
 
 
-	function clearResult(){
-		$resultadoBusca.empty();
-	}
+	//function postApi(){
+			
+
+		//$cep.keypress(function(e) {
+		//	stopRefreshing();
+		//	if(e.which == 13) {
+		//		myCep = $cep.val();
+		//	};		    
+		//});
+		
 
 
-	$.ajax({
-		type: "POST",
-		url: url,
-		dataType: "JSON",
-		success: function(data){
-			console.log(data);
-			},
-		error: function(){
-			alert("Error");
-		}
+		        $.ajax({
+				type: "POST",
+				url: url,
+				dataType: "JSON",
+				contentType: "application/json",
+				data: data,
+				success: function (data){
+						console.log(data);
+					},
+				error: function(){
+						alert("AJAX Error");
+					}
+			
 
 
-	});
-
-
-
+			});
+			
+	//};
 
 });
